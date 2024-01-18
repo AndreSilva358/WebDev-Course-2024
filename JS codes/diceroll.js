5f236801d67029b61cadd09b5d898506776c8f55
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-17 21:29:20 -0300

    dice roll and love calculator
    uploading two files concerning a dice roll rangin from 1 to 6, and a love calculator rangin from 1 to 100.

---
+++ JS codes/diceroll.js
@@ -0,0 +1,4 @@
+let n = Math.random(); //
+n = n * 7;
+n = Math.floor(n) + 1; //HERE, we raise the floor by one.
+console.log(n);
---
+++ JS codes/lovecalc.js
@@ -0,0 +1,7 @@
+prompt("First person's name:");
+prompt("Second person's name:");
+
+var love = Mathrandom();
+love = love * 101;
+love = Mathfloor(love) + 1;
+alert("Your love score is: "+love+"%.");
