3290ff9e28fe94b7f1c9831d916db6001a1390e5
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-20 18:55:20 -0300

    Create guestlist.js
    A simple code to check if a guest's name is on the guests list, with the purpose of trying out the .includes method

---
+++ JS codes/guestlist.js
@@ -0,0 +1,8 @@
+let guestName = prompt ("What is your name?");
+let guestList = [Angela, Jack, Pam, Arthas, Vancliff, LamarJackson];
+if (guestList.includes (guestName)) {
+    alert ("Welcome!")
+}
+else {
+    alert ("Sorry. Maybe next time.")
+}
