21a054640c86d1e6869c2f0fca48f22579c59dc3
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-20 12:33:44 -0300

    Update leapyear.js
    it is best to use 'const' for variables which you know won't change, and 'let' for the ones that will.

--- JS codes/leapyear.js
+++ JS codes/leapyear.js
@@ -1,7 +1,7 @@
 function isLeap(year) {
   let leapYear = year;
-  var notLeap = "Not leap year.";
-  var isLeap = "Leap year.";
+  const notLeap = "Not leap year.";
+  const isLeap = "Leap year.";
   if (leapYear % 4 === 0) {
     if (leapYear % 100 === 0) {
       if (leapYear % 400 === 0) {
