93fc60437bd68a3ccf000095831f0179bac04d2a
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-22 20:58:49 -0300

    Update fizzbuzz.js
    Updating the code by replacing the while loop with a for loop, which is more suitable.

--- JS codes/fizzbuzz.js
+++ JS codes/fizzbuzz.js
@@ -1,9 +1,8 @@
 var output = [];
-var count = 1;
 
 function fizzBuzz () {
 
-    while (count <= 100) {
+    for (var count=1; count<= 100; count++) {
 
  if (count % 3 === 0 && count % 5 ===0){
      output.push("FizzBuzz");
