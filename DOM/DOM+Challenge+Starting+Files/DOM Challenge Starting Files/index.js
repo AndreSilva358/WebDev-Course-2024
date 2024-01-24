b337976ae5ad8b8c70219ecc88989021ded5c479
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-24 14:31:40 -0300

    Document Object Model (DOM)

---
+++ DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+document.query.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+var third = document.query.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+third.innerHTML = "depresso espresso"
+
