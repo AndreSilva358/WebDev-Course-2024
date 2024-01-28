bf09fda63bf357976b4e4eeaa5877623f8c138bb
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-28 16:01:10 -0300

    Update index.js
    fixed to implement the listener on all buttons

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -1,3 +1,10 @@
-document.querySelectorAll("button").addEventListener("click", function () {
-    alert("I got clicked.");
-});
+//alternatively you can also declare a variable to control the number of drum buttons and pass it inside the for loop.
+//let numberDrums = document.querySelecotrAll(".drum").length;
+//we use .length to get the number of drums we have within the 'drum' class. 
+
+for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked.");
+    });
+}
