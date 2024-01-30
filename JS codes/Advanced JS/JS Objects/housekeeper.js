908e4074e3a9c62bc88e41b2bf4ae84a853f0be6
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-30 19:09:09 -0300

    Update housekeeper.js
    updated the housekeeper object by implementing a constructor function, which allows a sort of "streamlined production" of new housekeepers.

--- JS codes/Advanced JS/JS Objects/housekeeper.js
+++ JS codes/Advanced JS/JS Objects/housekeeper.js
@@ -1,6 +1,8 @@
-var housekeeper = {
-    name: Joanna,
-    age: 24,
-    previousExperience: true,
-    
+function HouseKeeper (name, age, previousExperience, yearsOfExperience) {
+    this.name = name;
+    this.age = age;
+    this.previousExperience = previousExperience;
+    this.yearsOfExperience = yearsOfExperience;
 }
+
+var houseKeeper1 = new HouseKeeper ("Joanna", 24, true, 2);
