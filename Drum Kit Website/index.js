88535dec3dbcf2e1cce9a89c4ec055620ebf97c8
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-30 15:53:03 -0300

    fix: cases
    fixed the cases which weren't being passed as strings, and replaced single quotes by double quotes.

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -8,41 +8,41 @@ for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
         var buttonInnerHTML = this.InnerHTML;
     
         switch (buttonInnerHTML) {
-            case w:
+            case "w":
             
-                var tom1 = new Audio('./sounds/tom1.mp3');
+                var tom1 = new Audio("./sounds/tom1.mp3");
                 tom1.play();
                 break;
                 
-            case a:
+            case "a":
             
-                var tom2 = new Audio('./sounds/tom2.mp3');
+                var tom2 = new Audio("./sounds/tom2.mp3");
                 tom2.play();
                 break;
             
-            case s:
+            case "s":
             
-                var tom3 = new Audio('./sounds/tom3.mp3');
+                var tom3 = new Audio("sounds/tom3.mp3");
                 tom3.play();
                 break;
                 
-            case d:
-                var tom4 = new Audio('./sounds/tom4.mp3');
+            case "d":
+                var tom4 = new Audio("sounds/tom4.mp3");
                 tom4.play();
                 break;
 
-            case j:
-                var snare = new Audio('./sounds/snare.mp3');
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
                 snare.play();
                 break;
 
-            case k:
-                var crash = new Audio("/sounds/crash.mp3");
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
                 crash.play();
                 break;
 
-            case l:
-                var kick = new Audio("/sounds/kick.mp3");
+            case "l":
+                var kick = new Audio("sounds/kick.mp3");
                 kick.play();
                 break;
 
