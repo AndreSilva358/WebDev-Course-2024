8f49543a560672b378a827f61698d55fccbd40c3
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-30 15:47:45 -0300

    Update index.js

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -5,6 +5,41 @@
 for (i = 0; i < document.querySelector(".drum").length ; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.buttonInnerHTML;
+    
+        switch (buttonInnerHTML) {
+            case w:
+            
+                var tom1 = new Audio('./sounds/tom1.mp3');
+                tom1.play();
+                break;
+        
+
+                
+            case a:
+            
+                var tom2 = new Audio('./sounds/tom2.mp3');
+                tom2.play();
+                break;
+            
+            case s:
+            
+                var tom3 = new Audio('./sounds/tom3.mp3');
+                tom3.play();
+                break;
+                
+            case d:
+                var tom4 = new Audio('./sounds/tom4.mp3');
+                tom4.play();
+                break;
+
+            case j:
+                var snare = new Audio('./sounds/snare.mp3');
+                snare.play();
+                break;
+            default:
+                break;
+        }
     }
     
     
@@ -13,5 +48,4 @@ for (i = 0; i < document.querySelector(".drum").length ; i++){
     
     
     
-    //var audio = new Audio('./sounds/crash.mp3');
-    //audio.play();    });
+}
