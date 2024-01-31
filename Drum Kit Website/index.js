e9db6d7e06b51bf1b73dddbf4e6417179d1b5cac
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-31 14:00:12 -0300

    minor fixes

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -1,11 +1,7 @@
-//alternatively you can also declare a variable to control the number of drum buttons and pass it inside the for loop.
-//let numberDrums = document.querySelecotrAll(".drum").length;
-//we use .length to get the number of drums we have within the 'drum' class. 
-
 for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        var buttonInnerHTML = this.InnerHTML;
+        var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
 
@@ -13,46 +9,46 @@ for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
     
     document.addEventListener("keydown", function(event){
         makeSound(event.key);
-    })
+    });
     
     function makeSound (key){
         
         switch (key) {
             case "w":
             
-                var tom1 = new Audio("./sounds/tom1.mp3");
+                var tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();
                 break;
                 
             case "a":
             
-                var tom2 = new Audio("./sounds/tom2.mp3");
+                var tom2 = new Audio("./sounds/tom-2.mp3");
                 tom2.play();
                 break;
             
             case "s":
             
-                var tom3 = new Audio("sounds/tom3.mp3");
+                var tom3 = new Audio("./sounds/tom-3.mp3");
                 tom3.play();
                 break;
                 
             case "d":
-                var tom4 = new Audio("sounds/tom4.mp3");
+                var tom4 = new Audio("./sounds/tom-4.mp3");
                 tom4.play();
                 break;
 
             case "j":
-                var snare = new Audio("sounds/snare.mp3");
+                var snare = new Audio("./sounds/snare.mp3");
                 snare.play();
                 break;
 
             case "k":
-                var crash = new Audio("sounds/crash.mp3");
+                var crash = new Audio("./sounds/crash.mp3");
                 crash.play();
                 break;
 
             case "l":
-                var kick = new Audio("sounds/kick.mp3");
+                var kick = new Audio("./sounds/kick-bass.mp3");
                 kick.play();
                 break;
 
