62c67ce3d018a19392cff235879d8b52777db784
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-01-31 10:50:32 -0300

    Update housekeeper.js
    added a method to the constructor function

--- JS codes/Advanced JS/JS Objects/housekeeper.js
+++ JS codes/Advanced JS/JS Objects/housekeeper.js
@@ -3,6 +3,11 @@ function HouseKeeper (name, age, previousExperience, yearsOfExperience) {
     this.age = age;
     this.previousExperience = previousExperience;
     this.yearsOfExperience = yearsOfExperience;
+    this.clean = function() {
+         alert("Cleaning in progress.");
+    }
 }
 
 var houseKeeper1 = new HouseKeeper ("Joanna", 24, true, 2);
+var houseKeeper2 = new HouseKeeper ("Elena", 3000 , true, 700);
+
