711ab2b237de2104860163e9eaff3423b75b08cc
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-01 15:29:00 -0300

    added pressed button style

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -63,5 +63,6 @@ for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
     
     function buttonAnimation (currentkey) {
         let activeButton = document.querySelector ("." + currentkey); //selects the current key and concatenates it with a dot
+        activeButton.classList.add("pressed")
     }
 }
