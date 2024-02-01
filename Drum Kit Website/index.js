e261e3aa231a80c2e6bdcb33d560906cb6d9226c
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-01 15:27:45 -0300

    Update index.js

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -1,14 +1,17 @@
 for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        var buttonInnerHTML = this.innerHTML;
+        let buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
-
+        buttonAnimation(buttonInnerHTML);
+        
     });
     
     document.addEventListener("keydown", function(event){
         makeSound(event.key);
+        buttonAnimation(event.key);
+
     });
     
     function makeSound (key){
@@ -16,47 +19,49 @@ for (i = 0; i < document.querySelectorAll(".drum").length ; i++){
         switch (key) {
             case "w":
             
-                var tom1 = new Audio("./sounds/tom-1.mp3");
+                let tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();
                 break;
                 
             case "a":
             
-                var tom2 = new Audio("./sounds/tom-2.mp3");
+                let tom2 = new Audio("./sounds/tom-2.mp3");
                 tom2.play();
                 break;
             
             case "s":
             
-                var tom3 = new Audio("./sounds/tom-3.mp3");
+                let tom3 = new Audio("./sounds/tom-3.mp3");
                 tom3.play();
                 break;
                 
             case "d":
-                var tom4 = new Audio("./sounds/tom-4.mp3");
+                let tom4 = new Audio("./sounds/tom-4.mp3");
                 tom4.play();
                 break;
 
             case "j":
-                var snare = new Audio("./sounds/snare.mp3");
+                let snare = new Audio("./sounds/snare.mp3");
                 snare.play();
                 break;
 
             case "k":
-                var crash = new Audio("./sounds/crash.mp3");
+                let crash = new Audio("./sounds/crash.mp3");
                 crash.play();
                 break;
 
             case "l":
-                var kick = new Audio("./sounds/kick-bass.mp3");
+                let kick = new Audio("./sounds/kick-bass.mp3");
                 kick.play();
                 break;
 
-            default:
+            default: console.log (buttonInnerHTML);
                 break;
         }
 
     }    
     
-    
+    function buttonAnimation (currentkey) {
+        let activeButton = document.querySelector ("." + currentkey); //selects the current key and concatenates it with a dot
+    }
 }
