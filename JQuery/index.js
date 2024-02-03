ca7aee5652f873c88938f4b32bd91b53e7bfbe2e
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-03 20:53:34 -0300

    epic update

--- JQuery/index.js
+++ JQuery/index.js
@@ -11,4 +11,13 @@ $("button").click(function(){
 
 $(document).keydown(function(event){
     $("h1").text(event.key);
-});
+}); //changes H1 based on keyboard input (keydown)
+
+$("button").on("mouseover", function(){
+	$("button").css("color", "red");
+    $("button").css("font-weight", "bold");
+}) // changes button colour when you hover the mouse over it.
+
+$("button").on("click", function(){
+	$("button").text("Dammit, you weren't supposed to actually click me.");
+}) 
