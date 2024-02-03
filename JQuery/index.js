82a9d1134db0cb731a4c328e64e4a4b8d3ae213c
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-03 20:42:17 -0300

    h1 changes with key pressing

--- JQuery/index.js
+++ JQuery/index.js
@@ -8,3 +8,7 @@ $("a").attr("href", "https://www.youtube.com/watch?v=yDp3cB5fHXQ&ab_channel=hbom
 $("button").click(function(){
 	$("h1").css("color", "purple");
 }); //adding an event listener that will listen for clicks on the buttons to change the colour of the H1
+
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+});
