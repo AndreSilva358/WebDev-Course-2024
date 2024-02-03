a053acd68287d1df73e52bd9c40f912b4f03bd41
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-03 20:35:55 -0300

    adding an event listener to change the colour of the H1

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,4 +1,10 @@
 //JQuery("h1");
-$("h1").css("big-title another-class");
+$("h1").click(function(){
+    $("h1").css("color", "violet");
+});
 
-$("a").attr("href", "https://www.chromiecraft.com/");
+$("a").attr("href", "https://www.youtube.com/watch?v=yDp3cB5fHXQ&ab_channel=hbomberguy");
+
+$("button").click(function(){
+	$("h1").css("color", "purple");
+}); //adding an event listener that will listen for clicks on the buttons to change the colour of the H1
