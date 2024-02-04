24f3611066e6b29f4e6ba91f5548a8389528c1de
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-04 19:45:09 -0300

    created the function
    As the world caves in - Matt Maltese

---
+++ Simon Game Challenge/game.js
@@ -0,0 +1,11 @@
+let gamePattern = [randomChosenColour];
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){ 
+    let randomNumber = Math.floor(Math.random() (0 * 4));
+    
+    let randomChosenColour = buttonColours[randomNumber];
+    
+    gamePattern.push(randomChosenColour);
+}
