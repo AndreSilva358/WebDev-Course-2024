00794abd622188a411c2d726645ddcb7e95f90b8
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-05 14:28:32 -0300

    Update game.js

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -2,24 +2,29 @@ let gamePattern = [];
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 
-let userClickedPattern =[];
+let userClickedPattern = [];
 
-$(".btn").on("click", function(){
-    let userChosenColour = $(this).attr(".id") 
-    userClickedPattern.push(userChosenColour);
+$(".btn").on("click", function () {
+  let userChosenColour = $(this).attr(".id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour); //will play sound according to the user's click
 });
 
-function nextSequence(){ 
-    let randomNumber = Math.floor(Math.random() (0 * 4));
-    
-    let randomChosenColour = buttonColours[randomNumber];
-    
-    gamePattern.push(randomChosenColour);
-    
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    
-    // what I thought was the way to play audio $("#"+randomChosenColour).audio.play("sounds/"+randomChosenColour+".mp3");
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random()(0 * 4));
+
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+
+  // what I thought was the way to play audio $("#"+randomChosenColour).audio.play("sounds/"+randomChosenColour+".mp3");
 }
 
+function playSound(name) {
+  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
