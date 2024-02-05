b961708e7b44b348f64aaefa139c4631dee7ca77
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-04 21:28:15 -0300

    tomfoolery
    - detect when any of the buttons are clicked and trigger a handler function.
    
    - store the id of the button that got clicked.
    
    - added the contents of the variable userChosenColour created to the end of the newly created userClickedPattern array.

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -2,6 +2,8 @@ let gamePattern = [];
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+let userClickedPattern =[];
+
 function nextSequence(){ 
     let randomNumber = Math.floor(Math.random() (0 * 4));
     
@@ -15,3 +17,8 @@ $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 // what I thought was the way to play audio $("#"+randomChosenColour).audio.play("sounds/"+randomChosenColour+".mp3");
 let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
 audio.play();
+
+$(".btn").on("click", function(){
+    let userChosenColour = $(this).attr(".id") 
+    userClickedPattern.push(userChosenColour);
+});
