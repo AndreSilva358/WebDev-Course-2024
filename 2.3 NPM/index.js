bb2dce2c284238bdb2196752c27377bf7d0b47cc
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-08 12:46:21 -0300

    imported superheroes package
    Now the console.log generates a silly name AND a superhero name.

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,4 +1,8 @@
 import generateName from "sillyname";
+import superheroes from "superheroes";
+
 var sillyname = generateName();
+const heroName = superheroes.random();
 
 console.log(`My name is ${sillyname}.`);
+console.log(`Also known as ${heroName}!`);
