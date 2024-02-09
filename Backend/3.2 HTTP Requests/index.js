ccaf2d6627ff2d1095ad6fdf5faa79e5641e2504
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-09 14:40:11 -0300

    added about and contact routes

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("Sal-u-tations!");
 })
 
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1>");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1><br><p>You can contact me at:andresilvaesilva099@gmail.com</p>");
+})
+
 app.listen(port, () => {
     console.log(`Server has been started on port ${port}.`);
 });
