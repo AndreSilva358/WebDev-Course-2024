c009febf214efa399cb45ef9d9f7bd39a90fc2d9
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-13 15:05:52 -0300

    add: app.use & app.post
    Added app.use to mount the middleware, in this case body parser, as well as a post method to be able to submit data.

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -7,8 +7,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
 
@@ -16,7 +27,3 @@ app.use((req, res, next) => {
   console.log('Time:', Date.now())
   next()
 })
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
