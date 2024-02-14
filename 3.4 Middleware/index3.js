2653198ba2f48b3663e4c6a5807d3b01948d1f73
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-14 15:19:37 -0300

    fix: typos
    haha dumb finger goes brrr

--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -1,11 +1,11 @@
-import express, { request } from "express";
+import express from "express";
 
 const app = express();
 const port = 3000;
 
 function logger (req, res, next) {
   console.log("Requested method:",req.method);
-  console.log("Requested URL". req.url);
+  console.log("Requested URL", req.url);
   next();
 }
 
