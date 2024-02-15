59b75a9e3539de80959524a6352875c8831eb209
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-15 13:54:05 -0300

    redid the code

--- 4.0_EJS/index.js
+++ 4.0_EJS/index.js
@@ -3,17 +3,16 @@ import express from "express";
 const port = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-    const today = new Date();
-    let day = today.getDay();
-
-    let date = "a weekday"
-    const comment = "time to work hard ye scallywag."
+app.get ("/", (req, res) => {
+    const currentDay = new Date()
+    const day = currentDay.getDay();
     
-    if (day === 0 || day === 6 ) {
-     date = "the weekend!"
-     comment = "happy resting sailor!"
+    let dateType = "a weekday";
+    const comment = "time to work hard ye scallywag."
+
+    if (day === 0 || day ===6) {
+        dateType = "the weekend";
+        comment = "time to get drinks and be marry sailor!";
     }
 
     res.render("index.ejs",
@@ -21,7 +20,6 @@ app.get("/", (req, res) => {
       comment: comment,
     });
 });
-    
 
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
