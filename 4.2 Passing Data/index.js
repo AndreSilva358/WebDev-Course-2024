8e0948d8a932a6b2f172c1ea9257f750a72df081
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-22 21:53:14 -0300

    Update index.js

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -7,8 +7,12 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {});
+res.render("index.ejs");
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+const lengthName = req.body['fname'].length+req.body['lname'].length;
+res.render("index.ejs", {numberOfLetters: lengthName});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
