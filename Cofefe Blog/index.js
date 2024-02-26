67f42d551c43770a223954bb2ecf3a43838dc205
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-26 20:18:36 -0300

    Creating initial files for the Cofefe blog

---
+++ Cofefe Blog/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+  
+const app = express();
+const port = 3000;
+
+app.use (bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+  res.render("/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
