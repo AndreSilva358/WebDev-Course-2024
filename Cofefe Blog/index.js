44c2e77c971a7f6e673eb712af73d1714a803dda
SilvaESilva099 <andre.silva358@fatec.sp.gov.br>
2024-02-26 21:02:49 -0300

    updated routes

--- Cofefe Blog/index.js
+++ Cofefe Blog/index.js
@@ -7,8 +7,16 @@ const port = 3000;
 app.use (bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
-  res.render("/public/index.html");
+  res.render("index.ejs");
 });
+
+app.get ("/about", (req, res) => {
+    res.render("about.ejs");
+  });
+  
+  app.get ("/posts", (req, res) => {
+    res.render("posts.ejs");
+  });
 
 app.post("/submit", (req, res) => {
   console.log(req.body);
